cb2ba5ad640c915752b00dc77d470b11819d97fc
CampbellMadsen <mad23011@byui.edu>
2025-02-27 22:58:35 -0700

    Added grade function

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,38 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string userInput = Console.ReadLine();
+        int grade = int.Parse(userInput);
+        var letter="";
+        if (grade < 60) {
+            letter = "F";
+        } else if (grade < 70) {
+            letter = "D";
+        } else if (grade < 80) {
+            letter = "C";
+        } else if (grade < 90) {
+            letter = "B";
+        } else {
+            letter = "A";
+        }
+        var symbol = "";
+        if (grade >= 90){
+            if (grade % 10 <3) {
+                symbol = "-";
+            }
+            } else if (grade >= 60){
+                if (grade % 10 <3) {
+                    symbol = "-";
+                } else if (grade % 10 >= 7){
+                    symbol = "+";
+                }
+            }
+        Console.WriteLine($"Your grade is {letter}{symbol}.");
+        if (grade >= 70) {
+            Console.WriteLine("You passed the class!");
+        } else {
+            Console.WriteLine("You did not pass the class.");
+        }
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,13 +1,28 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
+    public static int AddNumbers(int x, int y){
+        return x + y;
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
-        Console.WriteLine("Hello CSE 210!");
-        Console.WriteLine("Please enter your name: ");
-        string name = Console.ReadLine();
-        Console.WriteLine($"{name}");
+        // bool correctInput = false;
+        // do{
+        //     Console.Write("please input your age: ");
+        //     int age = int.Parse(Console.ReadLine());
+        //     if (age >= 0 && age < 120) {
+        //         Console.WriteLine($"Your age is: {age}");
+        //         correctInput=true;
+        //     } else correctInput = false;
+        // } while(!correctInput);
+
+        // Random newRandomNumber = new Random();
+        // int number = newRandomNumber.Next(1,1000);
+        // Console.WriteLine($"{number}");
+
+        int total = AddNumbers(100,200);
+        Console.WriteLine($"{total}");
     }
 }
