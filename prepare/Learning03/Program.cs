f8b5a51432edcf8fa8a9dd1467a8be91043c29b0
CampbellMadsen <mad23011@byui.edu>
2025-03-10 09:56:42 -0600

    Added learning03

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction myFraction = new Fraction();
+        myFraction.SetBottom(4);
+        myFraction.SetTop(2);
+        myFraction.GetFractionString();
+        myFraction.GetDecimalValue();
     }
 }
